2ee47c855946926fe8f18278e278640971ea6ddd
boryeira <jmanuel.jorquera@gmail.com>
2019-12-19 12:04:17 -0300

    testing

--- app/Http/Controllers/Orders/OrderController.php
+++ app/Http/Controllers/Orders/OrderController.php
@@ -14,10 +14,7 @@ class OrderController extends ApiController
     public function store(Request $request)
     {
         $user = Auth::user();
-      
-
         $items = $request->all();
-
         foreach($items as $item){
             
             $product = Product::find($item['id']);
@@ -30,41 +27,45 @@ class OrderController extends ApiController
             }
         }
 
-        $order = new Order;
-        $order->user_id = $user->id;
-        $order->quantity = array_sum($items);
-        $order->status = 2;
-        $order->save();
-  
-        foreach ($items as $key => $q) {
-              $product = Product::find($key);
-              $item = new OrderItem;
-              $item->order_id = $order->id;
-              $item->name = $product->name;
-              $item->product_id = $product->id;
-              $item->quantity = $q;
-              $item->amount = $product->value*$q;
-              $item->unit = $product->unit['id'];
-              $item->status = 1;
-              $item->img = $product->img;
-              $item->save();
-              $product->consumed = $product->consumed + $q;
-              if($product->available == 0){
-                $product->status = 2 ;
-              }
-              $product->save();
-            
-        }
-
-        $order->amount = $order->orderItems->sum('amount');
-        if($order->save()) {
-          return Redirect::route('orders.index');
-        } else {
-          return Redirect::back()->withErrors(array('db' => 'error en base de datos'));
-        }
-
         return Response()->json([
-            'data' => $request->all(),
-        ], 200);
+          'data' => 'verificado',
+      ], 200);
+
+        // $order = new Order;
+        // $order->user_id = $user->id;
+        // $order->quantity = array_sum($items);
+        // $order->status = 2;
+        // $order->save();
+  
+        // foreach ($items as $key => $q) {
+        //       $product = Product::find($key);
+        //       $item = new OrderItem;
+        //       $item->order_id = $order->id;
+        //       $item->name = $product->name;
+        //       $item->product_id = $product->id;
+        //       $item->quantity = $q;
+        //       $item->amount = $product->value*$q;
+        //       $item->unit = $product->unit['id'];
+        //       $item->status = 1;
+        //       $item->img = $product->img;
+        //       $item->save();
+        //       $product->consumed = $product->consumed + $q;
+        //       if($product->available == 0){
+        //         $product->status = 2 ;
+        //       }
+        //       $product->save();
+            
+        // }
+
+        // $order->amount = $order->orderItems->sum('amount');
+        // if($order->save()) {
+        //   return Redirect::route('orders.index');
+        // } else {
+        //   return Redirect::back()->withErrors(array('db' => 'error en base de datos'));
+        // }
+
+        // return Response()->json([
+        //     'data' => $request->all(),
+        // ], 200);
     }
 }
