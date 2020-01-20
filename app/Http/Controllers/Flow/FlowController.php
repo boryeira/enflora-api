3b78866c6ab3231c63dd9851425d4a71689b6a92
boryeira <jmanuel.jorquera@gmail.com>
2020-01-20 01:20:01 -0300

    last

--- app/Http/Controllers/Flow/FlowController.php
+++ app/Http/Controllers/Flow/FlowController.php
@@ -11,20 +11,18 @@ use App\Models\Orders\Order;
 class FlowController extends Controller
 {
     public $flow;
-
     public function __construct()
     {
         
-        // $this->flow = Flow::make('sandbox', [
-        //     'apiKey'    => '762E30CF-BA2B-4652-B187-613BE9L55983',
-        //     'secret'    => 'e387e7709f340d1f7e2adb4d47a5f6c25bf44c22',
-        // ]);
-        $this->flow   = Flow::make('production', [
-            'apiKey'    => '38EF2728-5354-4284-82BB-1L50EE34CC92',
-            'secret'    => 'cb0876ba65ec412e78492ed365f9477b61098e53',
-        ]);
+      $this->flow = Flow::make('production', [
+        'apiKey'    => '7B19A4CF-F041-40C4-9488-4180L75A6AAA',
+        'secret'    => '8a8c824cd4550b1ee4d581a1d3404d9d640638b0',
+      ]);
+      // $this->flow = Flow::make('sandbox', [
+      //         'apiKey'    => '367F3C6A-DEB8-46F7-89E5-32CLED2236B9',
+      //         'secret'    => '65d9f9656b478aaa7be72267bc33f40747f47c94',
+      //     ]);
     
-
     }
 
     public function orderPay(Order $order)
