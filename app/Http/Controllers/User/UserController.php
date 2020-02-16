8a56b3769ad4515527c5d3a19ae6f6c29d04dccb
boryeira <jmanuel.jorquera@gmail.com>
2020-02-16 00:50:23 -0300

    th

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -2,23 +2,23 @@
 
 namespace App\Http\Controllers\User;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Auth;
 use Response;
 
-class UserController extends Controller
+class UserController extends ApiController
 {
 
     public function fcmToken(Request $request)
     {
-      return response()->json(Auth::user());
-      Auth::user()->fcm_token = $request->fcmtoken;
-      if(Auth::user()->save()){
-        return response()->json('guardado-'.Auth::user()->fcm_token.' enviado:'.$request->fcmtoken.'request:'.$request->toJson(), 200);
-      } else {
-        return response()->json('no guardado', 401);
-      }
+
+        Auth::user()->fcm_token = $request->fcmtoken;
+        if(Auth::user()->save()){
+            return response()->json('guardado-'.Auth::user()->fcm_token.' enviado:'.$request->fcmtoken.'request:'.$request->toJson(), 200);
+        } else {
+            return response()->json('no guardado', 401);
+        }
 
     }
 }
